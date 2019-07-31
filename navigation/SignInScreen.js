83ceec3afa08fccf7f4a6cff515eee280e08bb52
Marcos Javier Riganti <marcos.riganti@gmail.com>
2019-07-31 12:33:19 +0200

    adding gradient buttons

--- navigation/SignInScreen.js
+++ navigation/SignInScreen.js
@@ -1,26 +1,52 @@
 import React from 'react';
-import { AsyncStorage, StatusBar, StyleSheet, View, Button } from 'react-native';
+import { AsyncStorage, StatusBar, StyleSheet, View, Button, Text } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
+
+// Local Components
 import Logo from '../components/Logo';
 import WelcomeLogo from '../components/WelcomeLogo';
 
+const styles = StyleSheet.create({
+  buttonText: {
+    backgroundColor: 'transparent',
+    fontSize: 15,
+    fontWeight: 'bold',
+    color: '#fff',
+  },
+});
 class SignInScreen extends React.Component {
   static navigationOptions = {
     headerTitle: <Logo />,
-    // title: 'testing',
   };
 
   render() {
     return (
       <View>
-        <WelcomeLogo />
         {/* Welcome png  */}
+        <WelcomeLogo />
         {/* Continue with LinkedIn */}
+        <LinearGradient
+          colors={['#4c669f', '#3b5998', '#192f6a']}
+          style={{ padding: 15, alignItems: 'center', borderRadius: 5 }}
+        >
+          <Text onPress={this._signInAsync} style={styles.buttonText}>
+            Sign in with Facebook
+          </Text>
+        </LinearGradient>
         {/* Continue With Phone Number */}
-        <Button title="Sign in!" onPress={this._signInAsync} />
+        <LinearGradient
+          colors={['#73BBCC', '#55A1F8', '#ADE75C']}
+          start={{ x: 0, y: 1 }}
+          end={{ x: 1, y: 1 }}
+          style={{ padding: 15, alignItems: 'center', borderRadius: 5 }}
+        >
+          <Text onPress={this._signInAsync} style={styles.buttonText}>
+            Sign in with Facebook
+          </Text>
+        </LinearGradient>
       </View>
     );
   }
-
   _signInAsync = async () => {
     await AsyncStorage.setItem('userToken', 'abc');
     this.props.navigation.navigate('App');
