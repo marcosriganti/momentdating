aaffaab0002afe9c1a1cf0999e5b0ff7a04d4d09
Marcos Javier Riganti <marcos.riganti@gmail.com>
2020-02-20 14:28:32 +0100

    logo update

--- components/Logo.js
+++ components/Logo.js
@@ -1,14 +1,23 @@
-import React from 'react';
-import { Image, StyleSheet } from 'react-native';
+import React from "react";
+import { Image, StyleSheet } from "react-native";
 
 export default function Logo(props) {
-  return <Image source={require('../assets/images/logo.png')} style={styles.navHeaderLogo} />;
+  return (
+    <Image
+      source={require("../assets/images/logo_gif.png")}
+      style={styles.navHeaderLogo}
+    />
+  );
 }
 
 const styles = StyleSheet.create({
   navHeaderLogo: {
-    width: 120,
-    height: 40,
-    resizeMode: 'contain',
-  },
+    width: 113,
+    height: 37,
+    marginVertical: 10,
+    paddingVertical: 10,
+    resizeMode: "contain",
+    borderColor: "red",
+    borderWidth: 1
+  }
 });
